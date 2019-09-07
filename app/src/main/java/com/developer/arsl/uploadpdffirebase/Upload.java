81909dbd5f61901d60ed72013c3a2b1e073a1002
Arslan Ali <marslanali789@gmail.com>
2019-09-07 21:57:31 +0500

    PDF Upload and Retrieve From Firebase Databse

---
+++ app/src/main/java/com/developer/arsl/uploadpdffirebase/Upload.java
@@ -0,0 +1,25 @@
+package com.developer.arsl.uploadpdffirebase;
+
+public class Upload {
+
+    public String name;
+    public String url;
+
+    // Default constructor required for calls to
+    // DataSnapshot.getValue(User.class)
+    public Upload() {
+    }
+
+    public Upload(String name, String url) {
+        this.name = name;
+        this.url = url;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+}
